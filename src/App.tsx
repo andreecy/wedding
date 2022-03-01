1cf4d50fc69c2737dd116b864f04e522390c8e13
Andree CY <andreechristianyulianto@gmail.com>
2022-03-01 15:12:06 +0700

    add page cover wip

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container mx-auto p-4 bg min-h-screen">
+      <p className="text-slate-400 font-semibold text-center font-text">SAVE THE DATE</p>
+      <p className="text-white text-center text-8xl font-script">Alsa</p>
+      <p className="text-white text-center text-8xl font-script">&</p>
+      <p className="text-white text-center text-8xl font-script">Andree</p>
+      <p className="text-slate-400 font-semibold text-center font-text">7 May 2022</p>
     </div>
+    </>
   );
 }
 
