d4863807bf5b196e484bf98b7cba805bb31c3b7c
Andree CY <andreechristianyulianto@gmail.com>
2022-03-16 22:15:25 +0700

    add ionicons nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import moment from 'moment';
 import { useEffect, useState } from 'react';
 import Page from './Page';
+import { HomeOutline, LocationOutline } from 'react-ionicons'
 
 function App() {
   return (
@@ -175,23 +176,29 @@ const Countdown = () => {
   )
 }
 
+
 const Nav = () => {
   return (
-    <div className="fixed block bg-gray-600 bottom-0 left-0 right-0 p-2">
-      <div className="flex justify-center">
-        <div className="bg-gray-500 p-2 w-[50px] h-[50px] rounded-xl lh-[50px]">
-	  img
-	</div>
-	<div className="bg-gray-500 p-2 w-[50px] h-[50px] rounded-xl lh-[50px]">
-	  img
-	</div>
-	<div className="bg-gray-500 p-2 w-[50px] h-[50px] rounded-xl lh-[50px]">
-	  img
-	</div>
-	<div className="bg-gray-500 p-2 w-[50px] h-[50px] rounded-xl lh-[50px]">
-	  img
-	</div>
+    <div className="fixed block bottom-0 left-0 right-0 p-2">
+      <div className="flex flex-row justify-center">
+        <div className="basis-full lg:basis-1/3 flex flex-row justify-center">
 
+		<div className="bg-gray-500 m-1 p-2 w-[50px] h-[50px] rounded-xl lh-[50px]">
+			<HomeOutline/>
+		</div>
+
+		<div className="bg-gray-500 m-1 p-2 w-[50px] h-[50px] rounded-xl lh-[50px]">
+		  	<LocationOutline/>
+		</div>
+		
+		<div className="bg-gray-500 m-1 p-2 w-[50px] h-[50px] rounded-xl lh-[50px]">
+		  img
+		</div>
+
+		<div className="bg-gray-500 m-1 p-2 w-[50px] h-[50px] rounded-xl lh-[50px]">
+		  img
+		</div>
+	</div>
       </div>
     </div>
   )
